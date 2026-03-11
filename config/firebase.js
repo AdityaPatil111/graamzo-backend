81711e2af79025437d9eac55a19a9d77e3fe3fa0
Niraj <nirajrandhir1111@gmail.com>
2026-03-11 13:30:16 +0530

    created env

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,15 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./serviceAccountKey.json", "utf8")
-);
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync("./serviceAccountKey.json", "utf8")
+//);
+// console.log("Service Account:", serviceAccount);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
